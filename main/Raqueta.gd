383b2b70a8f523e6b7bc0cf920b7dcbf0aa6e013
DeybisMelendez <pixelweb2007@gmail.com>
2020-08-04 14:22:22 -0600

    funcionalidad de musica añadida

--- main/Raqueta.gd
+++ main/Raqueta.gd
@@ -5,18 +5,37 @@ onready var Activate = $VBoxContainer/HBoxContainer2/Activate
 onready var Score = $VBoxContainer/HBoxContainer/Score
 onready var Music = $VBoxContainer/HBoxContainer3/Music
 onready var Mosquitos = $CenterContainer/Control.get_children()
-
+onready var AudioMusic = $AudioStreamPlayer
+onready var Anim = $AnimationPlayer
 var score = 0
 func _ready():
 	Rate.connect("button_up", self, "rate")
+
 func rate():
 	OS.shell_open("https://play.google.com/store/apps/details?id=com.damv.raqueta_mata_mosquitos")
+
+func get_gyroscope():
+	return Input.get_gyroscope().length()
 
 func _physics_process(_delta):
 	if Activate.pressed:
 		if Music.pressed:
-			pass
+			if AudioMusic.is_playing() and Anim.is_playing():
+				var gyroscope = get_gyroscope()
+				if gyroscope > 3:
+					AudioMusic.stream_paused = false
+					Anim.play("music")
+				else:
+					AudioMusic.stream_paused = true
+					Anim.play("stop_music")
+			else:
+				AudioMusic.play()
+				Anim.play("music")
 		else:
+			if AudioMusic.is_playing():
+				AudioMusic.stop()
+			if Anim.is_playing():
+				Anim.play("stop_music")
 			kill_mosquitos()
 	elif score != 0:
 		score = 0
@@ -26,7 +45,7 @@ func set_score(s):
 	Score.text = "Score: " + str(s)
 
 func kill_mosquitos():
-	var gyroscope = 10#Input.get_gyroscope().length()
+	var gyroscope = get_gyroscope()
 	var random
 	if gyroscope > 10:
 		random = randi()%3
